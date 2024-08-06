a36bffe5dbabd3f39f2fdc02ebf8f44f47cd1513
jobeyobey <hollands.joe@gmail.com>
2024-08-06 10:31:31 +0100

    navigate to and from articles, with basic styling

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,22 @@
-function Article({ topic, title, author, createdAt, votes, commentCount }) {
-    const date = new Date(Date.parse(createdAt));
+import { Link } from "react-router-dom";
+import { dateToString } from "../utils";
+
+function Article({
+    articleId,
+    topic,
+    title,
+    author,
+    createdAt,
+    votes,
+    commentCount,
+}) {
+    const date = dateToString(createdAt);
+
     return (
         <article className="list-article-container">
             <div className="list-article-top">
                 <p>{topic}</p>
-                <p>{date.toDateString()}</p>
+                <p>{date}</p>
             </div>
             <div>
                 <h3>{title}</h3>
@@ -13,6 +25,7 @@ function Article({ topic, title, author, createdAt, votes, commentCount }) {
             <div className="list-article-bot">
                 <p>Votes: {votes}</p>
                 <p>Comments: {commentCount}</p>
+                <Link to={`/article/${articleId}`}>To Article →</Link>
             </div>
         </article>
     );
--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getArticleById } from "../api";
+import "../styles/ArticlePage.css";
+import { dateToString } from "../utils";
 
 function ArticlePage() {
     const [article, setArticle] = useState({});
@@ -13,13 +15,21 @@ function ArticlePage() {
         });
     }, []);
 
+    const date = dateToString(article.created_at);
+
     return (
         <section className="page-container">
-            <img src={article.article_img_url} />
-            <p>{article.topic}</p>
-            <h1>{article.title}</h1>
-            <h2>{article.author}</h2>
-            <p>{article.created_at}</p>
+            <Link to="/">← Home</Link>
+            <img
+                src={article.article_img_url}
+                className="article-page-hero-image"
+            />
+            <div className="article-page-info">
+                <p>{article.topic}</p>
+                <p>{date}</p>
+            </div>
+            <h1 className="article-page-title">{article.title}</h1>
+            <p className="article-page-author">by {article.author}</p>
             <p>{article.body}</p>
         </section>
     );
