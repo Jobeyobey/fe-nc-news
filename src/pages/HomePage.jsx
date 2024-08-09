0588f1101b5f73bdd7272b277fab91c9808e7972
jobeyobey <hollands.joe@gmail.com>
2024-08-09 17:41:32 +0100

    sort artcles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,12 @@ import { getAllTopics, getAllUsers, getArticles } from "../api";
 import Article from "../components/Article";
 import PaginationButtons from "../components/PaginationButtons";
 import { useSearchParams } from "react-router-dom";
+import { prepareSearchParams } from "../utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faArrowUpShortWide,
+    faArrowDownShortWide,
+} from "@fortawesome/free-solid-svg-icons";
 
 function HomePage() {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -11,19 +17,23 @@ function HomePage() {
     const [articlesPage, setArticlesPage] = useState(1);
     const [isLoading, setIsLoading] = useState(true);
     const [topics, setTopics] = useState([]);
+    const [isOrderedDesc, setIsOrderedDesc] = useState(true);
 
     useEffect(() => {
         setIsLoading(true);
         getAllTopics().then((response) => {
             setTopics(response);
         });
-        getArticles(articlesPage, searchParams.get("topic")).then(
-            (response) => {
-                setArticles(response);
-                setIsLoading(false);
-            }
-        );
-    }, [articlesPage, searchParams]);
+        getArticles(
+            articlesPage,
+            searchParams.get("topic"),
+            searchParams.get("sort_by"),
+            searchParams.get("order")
+        ).then((response) => {
+            setArticles(response);
+            setIsLoading(false);
+        });
+    }, [articlesPage, searchParams, isOrderedDesc]);
 
     let topicOptions = [];
     if (topics.length > 0) {
@@ -56,7 +66,31 @@ function HomePage() {
     }
 
     function handleTopicChange(e) {
-        setSearchParams({ topic: e.target.value });
+        const params = prepareSearchParams(
+            e.target.value,
+            searchParams.get("sort_by"),
+            isOrderedDesc
+        );
+        setSearchParams(params);
+    }
+
+    function handleSortChange(e) {
+        const params = prepareSearchParams(
+            searchParams.get("topic"),
+            e.target.value,
+            isOrderedDesc
+        );
+        setSearchParams(params);
+    }
+
+    function handleOrderChange() {
+        const params = prepareSearchParams(
+            searchParams.get("topic"),
+            searchParams.get("sort_by"),
+            !isOrderedDesc
+        );
+        setSearchParams(params);
+        setIsOrderedDesc((currOrder) => !currOrder);
     }
 
     return (
@@ -65,16 +99,46 @@ function HomePage() {
                 <h1>Loading...</h1>
             ) : (
                 <>
-                    <select
-                        name="topics"
-                        id="topics"
-                        className="topic-select"
-                        onChange={handleTopicChange}
-                        value={searchParams.get("topic")}
-                    >
-                        <option value="All Topics">All Topics</option>
-                        {topicOptions}
-                    </select>
+                    <div className="search-options">
+                        <select
+                            name="topics"
+                            id="topics"
+                            className="query-select"
+                            onChange={handleTopicChange}
+                            value={searchParams.get("topic") || "all-topics"}
+                        >
+                            <option value="all-topics">All Topics</option>
+                            {topicOptions}
+                        </select>
+                        <select
+                            name="sort-by"
+                            id="sort-by"
+                            className="query-select"
+                            onChange={handleSortChange}
+                            value={searchParams.get("sort_by") || "date"}
+                        >
+                            <option value="date">Date</option>
+                            <option value="title">Title</option>
+                            <option value="topic">Topic</option>
+                            <option value="author">Author</option>
+                            <option value="votes">Popularity</option>
+                            <option value="comment_count">Comments</option>
+                        </select>
+                        {isOrderedDesc ? (
+                            <FontAwesomeIcon
+                                className="order-icon"
+                                icon={faArrowDownShortWide}
+                                onClick={handleOrderChange}
+                            />
+                        ) : (
+                            <FontAwesomeIcon
+                                className="order-icon"
+                                icon={faArrowUpShortWide}
+                                onClick={handleOrderChange}
+                            />
+                        )}
+                    </div>
+
                     {articleElements}
                     <PaginationButtons
                         totalCount={articles.article_count}
--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,11 @@ export function dateToString(createdAt) {
     const date = new Date(Date.parse(createdAt));
     return date.toDateString();
 }
+
+export function prepareSearchParams(topic, sort_by, isOrderedDesc) {
+    const params = {};
+    if (topic) params.topic = topic;
+    if (sort_by) params.sort_by = sort_by;
+    if (!isOrderedDesc) params.order = "ASC";
+    return params;
+}
